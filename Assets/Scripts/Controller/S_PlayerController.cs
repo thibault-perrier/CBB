ba7a1a9127b61e4c37eb9a4ce04f0003ac93bd63
Boushi33 <A.JOURDANNAUD@lahorde.school>
2024-05-21 15:02:27 +0200

    Added camera implementation
    - It's not possible to change the camera view by hiting a button
    - Only two views has been implemented : Arena and FPS.

---
+++ Assets/Scripts/Controller/S_PlayerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class S_PlayerController : MonoBehaviour
+{
+    private GameObject _cam;
+    private S_CameraView _cameraView;
+
+    private int _viewIndex = 0;
+
+    private void Start()
+    {
+        _cam = GameObject.Find("CameraManager");
+        _cameraView = _cam.GetComponent<S_CameraView>();
+    }
+
+    //Move the robot on the X axis
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log(context.ReadValue<float>());
+        }
+    }
+
+    //Rotate the robot on the Y axis
+    public void OnRotate(InputAction.CallbackContext context) 
+    {
+        if (context.performed)
+        {
+            Debug.Log(context.ReadValue<float>());
+        }
+    }
+
+    //Change the camera view
+    public void OnCangeView(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            int newValue = _viewIndex + (int)context.ReadValue<float>();
+            _viewIndex = newValue > 3 ? 0 : newValue < 0 ? 3 : newValue;
+
+            _cameraView.SetViewType((S_CameraView.ViewType)_viewIndex);
+        }
+    }
+}
