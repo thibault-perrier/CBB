398b221faed555d6303c09ee7e81d416711f29d1
mateo809 <150682933+mateo809@users.noreply.github.com>
2024-05-21 17:27:42 +0200

    Shop
    -scroll items and anim

---
+++ Assets/Scripts/Shop/S_SwipeController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_SwipeController : MonoBehaviour
+{
+    [SerializeField] int _maxPage;
+    int _currentPage;
+    Vector3 _targetPos;
+    [SerializeField] Vector3 _pageStep;
+    [SerializeField] RectTransform _levelPageRect;
+
+    [SerializeField] float _tweenTime;
+    [SerializeField] LeanTweenType _tweenType;
+
+    private void Awake()
+    {
+        _currentPage = 1;
+        _targetPos = _levelPageRect.localPosition;
+    }
+    public void Next()
+    {
+        if (_currentPage < _maxPage)
+        {
+            _currentPage++;
+            _targetPos += _pageStep;
+            MovePage();
+
+        }
+    }
+
+    public void Previous()
+    {
+        if (_currentPage > 1)
+        {
+            _currentPage--;
+            _targetPos -= _pageStep;
+            MovePage();
+
+        }
+
+    }
+
+    public void MovePage()
+    {
+        _levelPageRect.LeanMoveLocal(_targetPos, _tweenTime).setEase(_tweenType);
+    }
+
+}
