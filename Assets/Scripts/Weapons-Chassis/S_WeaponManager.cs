628f0c6569e0e716a3a9f982777500c55b29b36d
hdenise-lahorde <60253055+Auros1805@users.noreply.github.com>
2024-05-24 17:38:23 +0200

    update Weapon manager

--- Assets/Scripts/Weapons-Chassis/S_WeaponManager.cs
+++ Assets/Scripts/Weapons-Chassis/S_WeaponManager.cs
@@ -9,12 +9,14 @@ public class S_WeaponManager : MonoBehaviour, I_Damageable
 {
     private int _life;
     private int _damage;
-    private bool _activeWeapon;
     [SerializeField] private S_WeaponData _data;
     [SerializeField] private State _state = State.ok;
     [SerializeField] private int _lifeBrakePoint = 15;    //In pourcent life level
+    [SerializeField] private bool _shotWeapon = false;
     private float _brakePoint;
     private Rigidbody _rb;
+    private bool _attacking = false;
+
 
     public enum State
     {
@@ -59,7 +61,6 @@ public class S_WeaponManager : MonoBehaviour, I_Damageable
         _rb.mass = _data.Mass;
         _life = _data.MaxLife;
         _damage = _data.Damage;
-        _activeWeapon = _data.Active;
         _brakePoint = (_lifeBrakePoint * _data.MaxLife) / 100;
     }
 
@@ -68,7 +69,12 @@ public class S_WeaponManager : MonoBehaviour, I_Damageable
         _life = _data.MaxLife;
         _state = State.ok;
     }
-    
 
-
+    private void OnCollisionStay(Collision collision)
+    {
+        if(_attacking && collision.gameObject.TryGetComponent(out I_Damageable damageable))
+        {
+            damageable.TakeDamage(_damage);
+        }
+    }
 }
