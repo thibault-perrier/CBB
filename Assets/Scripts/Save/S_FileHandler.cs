3ddeb1f76809cc711abeb6eff652a4342c128965
mateo809 <150682933+mateo809@users.noreply.github.com>
2024-05-27 15:57:29 +0200

    Save And Load Scripts
    -S_DataGame
    -S_FileHandler

---
+++ Assets/Scripts/Save/S_FileHandler.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_FileHandler: MonoBehaviour
+{
+   
+    public void SaveInventory()
+    {
+        string inventoryData = JsonUtility.ToJson(S_DataGame.instance.inventory);
+        string filePath = Application.persistentDataPath + "/InventoryData.json";
+        Debug.Log(filePath);
+        System.IO.File.WriteAllText(filePath, inventoryData);
+        Debug.Log("Sauvegarde effectué");
+    }
+
+    public void LoadInventory()
+    {
+        string filePath = Application.persistentDataPath + "/InventoryData.json";
+        string inventoryData = System.IO.File.ReadAllText(filePath);
+
+        S_DataGame.instance.inventory = JsonUtility.FromJson<InventorySaver>(inventoryData);
+        Debug.Log("Changement effectué");
+    }
+
+    public void SaveTournament()
+    {
+        string tournamentData = JsonUtility.ToJson(S_DataGame.instance.tournament);
+        string filePath = Application.persistentDataPath + "/TournamentData.json";
+        Debug.Log(filePath);
+        System.IO.File.WriteAllText(filePath, tournamentData);
+        Debug.Log("Sauvegarde effectué");
+    }
+
+    public void LoadTournament()
+    {
+        string filePath = Application.persistentDataPath + "/TournamentData.json";
+        string tournamentData = System.IO.File.ReadAllText(filePath);
+
+        S_DataGame.instance.tournament = JsonUtility.FromJson<TournamentSaver>(tournamentData);
+        Debug.Log("Changement effectué");
+    }
+}
