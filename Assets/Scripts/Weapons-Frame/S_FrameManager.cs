360d00a22b24c47e55aec7fa5311ad8c9e1df79d
hdenise-lahorde <60253055+Auros1805@users.noreply.github.com>
2024-06-03 14:56:22 +0200

    correction

--- Assets/Scripts/Weapons-Frame/S_FrameManager.cs
+++ Assets/Scripts/Weapons-Frame/S_FrameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class S_FrameManager : MonoBehaviour, I_Damageable
 {
     private int _life;
-    [SerializeField] private S_WeaponData _data;
+    [SerializeField] private S_FrameData _data;
     private Rigidbody _rb;
     [SerializeField] private List<GameObject> _weaponHookPoints;
     [SerializeField] private bool _player = false;
