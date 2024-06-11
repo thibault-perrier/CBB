abe2bb048a636d2604cb5050fdfff42a849a7af5
Mathieu Laval <m.laval@lahorde.school>
2024-06-11 10:54:57 +0200

    add comentary in frame manager

--- Assets/Scripts/Weapons-Frame/S_FrameManager.cs
+++ Assets/Scripts/Weapons-Frame/S_FrameManager.cs
@@ -1,21 +1,31 @@
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class S_FrameManager : MonoBehaviour, I_Damageable
 {
     private List<S_WeaponManager> _weaponManagers;
     private Rigidbody _rb;
-    
-    [SerializeField] private float _life;
-    [SerializeField] private S_FrameData _data;
-    [SerializeField] private List<GameObject> _weaponHookPoints;
 
+    public event Action<S_FrameManager> OnDie;
+    
+    [SerializeField, Tooltip("the current health point of the frama")] 
+    private float _life;
+    [SerializeField, Tooltip("all statistic in the frame")] 
+    private S_FrameData _data;
+    [SerializeField, Tooltip("All kook point for get weapons")] 
+    private List<GameObject> _weaponHookPoints;
+
+    /// <summary>
+    /// return the number of hook point
+    /// </summary>
     public int NBWeaponHookPoints
     {
         get { return _weaponHookPoints.Count; }
     }
+    /// <summary>
+    /// return all the weapon in the frame
+    /// </summary>
     public List<S_WeaponManager> Weapons
     {
         get => _weaponManagers;
@@ -28,8 +38,10 @@ public class S_FrameManager : MonoBehaviour, I_Damageable
         _life = _data.MaxLife;
     }
     
-    public event Action<S_FrameManager> OnDie;
-
+    /// <summary>
+    /// detect if there is any weapon is always OK
+    /// </summary>
+    /// <returns>return True if all weapon is not OK</returns>
     public bool AllWeaponIsBroken()
     {
         foreach (var weapon in _weaponManagers)
@@ -40,6 +52,9 @@ public class S_FrameManager : MonoBehaviour, I_Damageable
 
         return true;
     }
+    /// <summary>
+    /// Get all weapon with hook points
+    /// </summary>
     public void SelectWeapons()
     {
         foreach (GameObject gameObject in _weaponHookPoints)
@@ -51,18 +66,29 @@ public class S_FrameManager : MonoBehaviour, I_Damageable
             }
         }
     }
+    /// <summary>
+    /// take damage in the current frame
+    /// </summary>
+    /// <param name="amount"></param>
     public void TakeDamage(float amount)
     {
+        // remove life and if the life is lower or equal at 0 is die
         _life -= amount;
         if (_life <= 0f)
         {
             Die();
         }
     }
+    /// <summary>
+    /// Start when the life of the current frame is lower or equal at 0
+    /// </summary>
     public void Die()
     {
         OnDie?.Invoke(this);
     }
+    /// <summary>
+    /// repear the frame, set the life with the data
+    /// </summary>
     public void Repear()
     {
         _life = _data.MaxLife;
