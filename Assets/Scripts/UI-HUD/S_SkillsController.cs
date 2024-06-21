a379c9ad038f9f9bacff8559536d93ae2e159c08
Mathieu Laval <m.laval@lahorde.school>
2024-06-21 10:15:51 +0200

    fix the skills controller
    unBind Listener when it is disable

--- Assets/Scripts/UI-HUD/S_SkillsController.cs
+++ Assets/Scripts/UI-HUD/S_SkillsController.cs
@@ -27,8 +27,59 @@ public class S_SkillsController : MonoBehaviour
     [SerializeField]
     private S_Skill skillRight;
 
+    private S_FrameManager _frameManger;
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+        DisableAllListener();
+    }
+
+    private void DisableAllListener()
+    {
+        List<S_Skill> skills = new()
+        {
+            skillLeft,
+            skillUp,
+            skillRight,
+            skillDown,
+        };
+        List<UnityAction> actionCooldown = new()
+        {
+            ListenerCooldownMashLeft,
+            ListenerCooldownMashUp,
+            ListenerCooldownMashRight,
+            ListenerCooldownMashDown,
+        };
+        List<UnityAction> actionDestroy = new()
+        {
+            ListenerDestroyMashLeft,
+            ListenerDestroyMashUp,
+            ListenerDestroyMashRight,
+            ListenerDestroyMashDown
+        };
+
+        foreach (var skill in skills.Select((value, index) => new { index, value }))
+        {
+            var currentSkill = skill.value;
+
+            if (skill.index <= _frameManger.Weapons.Count - 1)
+            {
+                currentSkill.Weapon = _frameManger.Weapons[skill.index];
+                currentSkill.WeaponIcon.sprite = currentSkill.Weapon.Data.WeaponSrite;
+                currentSkill.Weapon.AttackingEnd.RemoveListener(actionCooldown[skill.index]);
+                currentSkill.Weapon.WeaponDestroy.RemoveListener(actionDestroy[skill.index]);
+            }
+            else
+            {
+                currentSkill.WeaponIcon.gameObject.SetActive(false);
+            }
+        }
+    }
     public void InitializeSkills(S_FrameManager frameManager)
     {
+        _frameManger = frameManager;
+
         List<S_Skill> skills = new() 
         { 
             skillLeft, 
